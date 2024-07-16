8d067d22e2af554bb10b32d68ba2ea26c8164ccf
lukasolsen <lukmarwil@gmail.com>
2024-07-16 12:16:44 +0200

    Removed js folder

--- js/src/controller/scan.controller.ts
+++
@@ -1,6 +0,0 @@
-import { prisma } from "~/data-source";
-import { Secret } from "~/types/secret";
-
-export const addSecret = async (secret: Secret) => {
-  const { pattern } = secret;
-}
--- js/src/routes/index.ts
+++
@@ -1,1 +0,0 @@
-export * as scanRouter from "./scan";
